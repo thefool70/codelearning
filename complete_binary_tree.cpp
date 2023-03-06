54ed513590fb1b3882f0ef191abc350dd32d1591
the_fool <2275970643@qq.com>
2023-03-06 18:35:43 +0800

    通过"提前影响"生成最大堆

--- complete_binary_tree.cpp
+++ complete_binary_tree.cpp
@@ -22,8 +22,42 @@ for(int i=1;i<=_size;i++)
 }
 }
 
+/*void tree_sort(int*a,int n)
+{
+    if(2*n<=_size) tree_sort(a,2*n);
+    if(2*n<=_size-1) tree_sort(a,2*n+1);
+    if(2*n<=_size-1&&a[n]<a[2*n+1])  swap(a[2*n+1],a[n]);
+    if(2*n<=_size&&a[n]<a[2*n]) swap(a[2*n],a[n]);
+}*/
+
+/*bool cmp(int a,int b)
+{
+    if (a>b) return true;
+    else return false;
+}*/
+
+void tree_sort(int*a,int n)
+{   if(n>_size/2) return;
+    int l=2*n;
+    int r=2*n+1;
+    int largest=n;
+    if(l<=_size&&a[n]<a[l])  largest=l;
+    else largest=n;
+    if(r<=_size&&a[l]<a[r]) largest=r;
+    else largest=l;
+    if(largest!=n)
+    {
+        swap(a[n],a[largest]);
+        tree_sort(a,largest);
+    }
+}
+
+
 int main()
 {
   int*a=tree_make();
+  tree_print(a);
+  cout<<endl;
+  for(int i=_size/2;i>1;i--) tree_sort(a,i);
   tree_print(a);
 }
